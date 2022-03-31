4942de227ade2d1ad2bcfdac9df698765f409463
laaouissi <laaouissi@mediamarkt.es>
2022-03-31 09:57:40 +0200

    updated settings_bloc.dart

--- lib/src/settings/bloc/settings_bloc.dart
+++ lib/src/settings/bloc/settings_bloc.dart
@@ -3,11 +3,11 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
 part 'settings_event.dart';
-
 part 'settings_state.dart';
 
 class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
   SettingsBloc() : super(const SettingsState(ThemeMode.system)) {
-    on<ChangeThemeEvent>((event, emit) => emit(SettingsState(event.themeMode)));
+    on<ChangeThemeEvent>(
+        (event, emit) => emit(const SettingsState(ThemeMode.dark)));
   }
 }
